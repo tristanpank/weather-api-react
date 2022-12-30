36c0500d1c0648474a2a9ae756f505974dea91b9
tristanpank <tristanpank@gmail.com>
2022-12-30 10:53:19 -0500

    Create timezone selecter

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,12 +1,67 @@
 import { celciusToFarenheight } from "./Data";
+import { useState } from "react";
+
+function TimezoneDropdown({ setTimezone }) {
+  const timezonesArray = [
+    "IDLW",
+    "NT",
+    "HST",
+    "AKST",
+    "PST",
+    "MST",
+    "CST",
+    "EST",
+    "AST",
+    "ART",
+    "AT",
+    "WAT",
+    "GMT",
+    "CET",
+    "EET",
+    "MSK",
+    "AMT",
+    "PKT",
+    "OMSK",
+    "KRAT",
+    "JST",
+    "AEST",
+    "SAKT",
+    "NZST",
+  ]
+  const selectTimezoneItems = timezonesArray.map(timezone => <option value={timezone} key={timezone}>{timezone}</option>)
+
+  function handleTimezoneChange(e) {
+    setTimezone(e.target.value);
+  }
+
+  return (
+    <form>
+      <label htmlFor="timezones">
+        Choose a Timezone: 
+        <select id="timezones" onChange={handleTimezoneChange}>
+          {selectTimezoneItems}
+        </select>
+      </label>
+    </form>
+  )
+}
+
 
 export default function Forecast({ forecastData, isCelcius }) {
+  const [timezone, setTimezone] = useState(null);
+  // Take forecast data and extracts first 8 intervals (1 day)
+  // Converts to object with just time and temp
   const nextDay = [forecastData.list.slice(0, 8)]
   const dayData = nextDay[0].map(day => {
-    const time = new Date(day.dt * 1000);
+    let time = new Date(day.dt * 1000);
     let temp = String((day.main.temp - 273.15).toFixed(2)) + " °C";
     if (!isCelcius) {
       temp = celciusToFarenheight(temp.slice(0, -3));
+    }
+    if (timezone !== null) {
+      time = time.toLocaleTimeString("GMT", {timeZone: timezone});
+    } else {
+      time = time.toLocaleTimeString();
     }
     return {"time": time, "temp": temp};
   });
@@ -15,5 +70,9 @@ export default function Forecast({ forecastData, isCelcius }) {
   //   return date.toLocaleTimeString("en-US");
   // })
   console.log(dayData);
-  console.log(dayData[0].time.toLocaleTimeString("en-US"))
+  // console.log(dayData[0].time.toLocaleTimeString("GMT", {timeZone: "MST"}))
+
+  return (
+    <TimezoneDropdown setTimezone={setTimezone} />
+  )
 }
